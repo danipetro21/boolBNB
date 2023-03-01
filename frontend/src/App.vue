f682bfe57ce45cd3fbb4a1f52a39245e449f1cc3
Jessica <jesscara.993@gmail.com>
2023-03-01 12:59:35 +0100

    favicon added, font imported

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,18 @@
 <script>
 import { RouterView } from 'vue-router';
 
-export default{
+export default {
 
 }
 </script>
 
 <template>
- <div>
-  <router-view/>
- </div>
-
-
+  <div>
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
-
+@use './style/general.scss' as *;
+@use './style/partials/_variables' as *;
 </style>
