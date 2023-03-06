641aae26f138727232f8a99f8fac5c39f72d80c3
Jessica <jesscara.993@gmail.com>
2023-03-06 12:31:13 +0100

    exp-date working

--- server/database/migrations/2023_03_02_153119_create_property_sponsorship_table.php
+++ server/database/migrations/2023_03_02_153119_create_property_sponsorship_table.php
@@ -4,31 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('property_sponsorship', function (Blueprint $table) {
-            $table->id();
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('property_sponsorship', function (Blueprint $table) {
+      $table->id();
 
-            $table->date('exp_date');
+      $table->date('exp_date')->nullable();
+      ;
 
-            $table->timestamps();
-        });
-    }
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('property_sponsorship');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('property_sponsorship');
+  }
 };
